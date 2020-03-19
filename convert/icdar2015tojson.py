866ec40cba15364f34023aeb199c68760cb4762c
zhoujun <572459439@qq.com>
2020-03-19 09:19:51 +0800

    更新cocotext到v2

--- convert/icdar2015tojson.py
+++ convert/icdar2015tojson.py
@@ -1,3 +1,18 @@
 # -*- coding: utf-8 -*-
 # @Time    : 2020/3/18 14:12
 # @Author  : zhoujun
+
+def cvt(gt_path, save_path):
+    """
+    将icdar2015格式的gt转换为json格式
+    :param gt_path:
+    :param save_path:
+    :return:
+    """
+    pass
+
+
+if __name__ == '__main__':
+    gt_path = ''
+    save_path = ''
+    cvt(gt_path,save_path)
